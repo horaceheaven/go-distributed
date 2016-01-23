b5dcd2ab94df50fa83df7846e6c90ba6d4d8d68f
horaceheaven <hheaven@medullan.com>
2016-01-23 14:11:06 -0400

    Created dto and included a buffer and message encoding

---
+++ src/distributed/dto/dto.go
@@ -0,0 +1,15 @@
+package dto
+import (
+	"time"
+	"encoding/gob"
+)
+
+type SensorMessage struct {
+	Name string
+	Value float64
+	Timestamp time.Time
+}
+
+func init() {
+	gob.Register(SensorMessage{})
+}
